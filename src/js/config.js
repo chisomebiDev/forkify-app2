d85bb4a798a2cbd17966bece6c77b3f9ba6a4aff
chisomebi onwunyi <onwunyichisom@gmail.com>
2021-11-21 14:55:16 +0100

    Second commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '80f48897-94ad-42e8-8446-aaff3a64b7f2';
 export const MODAL_CLOSE_SEC = 2.5;
